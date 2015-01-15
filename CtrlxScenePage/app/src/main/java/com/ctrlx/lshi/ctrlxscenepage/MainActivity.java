543f7a80bd1ba4efdc2e02e24114efa55e070c8a
geoxxu <zzztigerxu@gmail.com>
2015-01-15 03:17:26 -0500

    sidemenu test

--- CtrlxScenePage/app/src/main/java/com/ctrlx/lshi/ctrlxscenepage/MainActivity.java
+++ CtrlxScenePage/app/src/main/java/com/ctrlx/lshi/ctrlxscenepage/MainActivity.java
@@ -1,17 +1,46 @@
 package com.ctrlx.lshi.ctrlxscenepage;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
     private Button mBtnTv;
-    private Button mTheatre;
+    private LinearLayout mLayout;
+
+    /**
+     * 侧滑布局对象，用于通过手指滑动将左侧的菜单布局进行显示或隐藏。
+     */
+    private SlidingLayout slidingLayout;
+
+    private Button menuButton;
+    /**
+     * 放在content布局中的ListView。
+     */
+    private ListView contentListView;
+
+    /**
+     * 作用于contentListView的适配器。
+     */
+    private ArrayAdapter<String> contentListAdapter;
+
+    /**
+     * 用于填充contentListAdapter的数据源。
+     */
+    private String[] contentItems = { "Content Item 1", "Content Item 2", "Content Item 3",
+            "Content Item 4", "Content Item 5", "Content Item 6", "Content Item 7",
+            "Content Item 8", "Content Item 9", "Content Item 10", "Content Item 11",
+            "Content Item 12", "Content Item 13", "Content Item 14", "Content Item 15",
+            "Content Item 16" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +48,36 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mBtnTv = (Button) findViewById(R.id.btn_tv);
+
         mBtnTv.setOnClickListener(this);
 
-        //this  button is to control the wemo switch
-        mTheatre = (Button) findViewById(R.id.btn_theatre);
-        mTheatre.setOnClickListener(this);
+        slidingLayout = (SlidingLayout) findViewById(R.id.slidingLayout);
+        menuButton = (Button) findViewById(R.id.menuButton);
+        /*contentListView = (ListView) findViewById(R.id.contentList);
+        contentListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                contentItems);
+        contentListView.setAdapter(contentListAdapter);*/
+        mLayout = (LinearLayout) findViewById(R.id.content);
+        // 将监听滑动事件绑定在contentListView上
+        slidingLayout.setScrollEvent(mLayout);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (slidingLayout.isLeftLayoutVisible()) {
+                    slidingLayout.scrollToRightLayout();
+                } else {
+                    slidingLayout.scrollToLeftLayout();
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_tv:
-                Intent intent = new Intent(MainActivity.this, TvActivity.class);
+                Intent intent = new Intent(MainActivity.this, TvPanelActivity.class);
                 startActivity(intent);
-                this.finish();
                 break;
         }
     }
